6b705ff074506908c5cf1d5ff53b4a3173a9ea0c
Ray Brennan <ray@quitealright.com>
2024-05-03 13:59:35 +0100

    Removed the last of the Pokemon scraps

--- Components/Base/Constants.cs
+++ Components/Base/Constants.cs
@@ -5,7 +5,7 @@
         public const string DatabaseFileName = "doggy.db3";
         public static string DatabasePath = Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
 
-        public static string RestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://pokeapi.co/api/v2/pokemon/" : "https://pokeapi.co/api/v2/pokemon/";
+
         public static string RestUrlDogs = DeviceInfo.Platform == DevicePlatform.Android ? "https://dog.ceo/api/" : "https://dog.ceo/api/";
 
 
